0a3020ae1dbdd9f297afa7140dd9f1cab7ce2299
PRASHANT THAKUR <prashant101007@gmail.com>
2024-08-12 15:38:16 +0530

    seperating css and js and html

---
+++ scripts/calculator.js
@@ -0,0 +1,16 @@
+let calculation = localStorage.getItem('calculation')||'';
+      displaycalculation();
+      function updateCalculation(value){
+        calculation+=value;
+        
+        
+
+        displaycalculation();
+        localStorage.setItem('calculation',calculation);
+        
+
+      }
+      function displaycalculation(){
+        document.querySelector('.js-calculation')
+        .innerHTML=calculation;
+      }
