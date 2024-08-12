ed9c649a464f946b4ea37458bc5ef40a16949ed6
PRASHANT THAKUR <prashant101007@gmail.com>
2024-08-12 17:11:19 +0530

    making to do list basics

--- scripts/11-to-do-list.js
+++ scripts/11-to-do-list.js
@@ -1,12 +1,37 @@
-const todoList=[];
+const todoList=['make food','make room'];
+
+rendertodolist();
+
+function rendertodolist(){
+  let todoisHTML ='';
+  for(let i=0;i<todoList.length;i++){
+    const todo = todoList[i];
+    const html=`<p>${todo}</p>`;
+    todoisHTML+=html;
+  }
+  console.log(todoisHTML);
+  document.querySelector('.js-to-list')
+  .innerHTML = todoisHTML;
+
+}
+
+
+
+
+
+
+
 function addToDo(){
   
   const nameelement=document.querySelector('.js-input-box');
   const name=nameelement.value;
   todoList.push(name);
-  
   console.log(todoList);
+ 
+  
+  
   nameelement.value='';
+  rendertodolist();
 
 
 
