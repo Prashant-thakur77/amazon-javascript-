8dd8586e0e9167d836f53d4ef3966eb1f836c043
PRASHANT THAKUR <prashant101007@gmail.com>
2024-08-13 16:22:53 +0530

    using for each method in to do list

---
+++ scripts/12-to-do-list.js
@@ -0,0 +1,63 @@
+ const todoList=[{
+  name:'make dinner',
+  duedate:'2024-12-22'
+}];
+
+rendertodolist();
+
+function rendertodolist(){
+  let todoisHTML ='';
+  
+  todoList.forEach(function(todoObject,index){
+    
+    const {name,duedate}=todoObject;
+   
+    const html=`
+    
+    <div> ${name}</div>
+    <div> ${duedate}</div>
+
+    
+    <button onclick="
+    todoList.splice(${index},1);
+    rendertodolist();
+
+    " class="delete-button">Delete</button>`
+    ;
+    todoisHTML+=html;
+  })
+
+  document.querySelector('.js-to-list')
+  .innerHTML = todoisHTML;
+
+  }
+ 
+
+
+
+
+
+
+
+function addToDo(){
+  
+  const nameelement=document.querySelector('.js-input-box');
+  const name=nameelement.value;
+  const duedateelement=document.querySelector('.js-date-selector')
+  const duedate=duedateelement.value;
+
+  todoList.push({
+    name,
+    duedate
+  });
+ 
+ 
+  
+  
+  nameelement.value='';
+  duedateelement.value='';
+  rendertodolist();
+
+
+
+}
