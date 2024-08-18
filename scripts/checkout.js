70357afeaf2efa0c5ecf7cb7e17dcacca962ec8f
PRASHANT THAKUR <prashant101007@gmail.com>
2024-08-18 17:12:33 +0530

    delete products drom html

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import{cart} from '../data/cart.js';
+import{cart,removeFromCart} from '../data/cart.js';
 import{products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 let cartSummaryHTML;
@@ -16,7 +16,7 @@ cart.forEach((cartItem) =>{
 
   });
   cartSummaryHTML +=
-  `<div class="cart-item-container">
+  `<div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: Friday, August 23
       </div>
@@ -40,7 +40,7 @@ cart.forEach((cartItem) =>{
               Update
 
             </span>
-            <span class="delete-button">
+            <span class="delete-button js-delete-link" data-product-id="${matchingProduct.id}">
               Delete
             </span>
 
@@ -116,4 +116,18 @@ cart.forEach((cartItem) =>{
 
 });
 document.querySelector('.js-order-summary').innerHTML=cartSummaryHTML;
+
+document.querySelectorAll('.js-delete-link')
+  .forEach((link)=> {
+    link.addEventListener('click',()=>{
+      const productId = link.dataset.productId;
+      removeFromCart(productId);
+      const container=document.querySelector(`.js-cart-item-container-${productId}`);
+      container.remove();
+      
+
+    });
+  });
+
+
 
