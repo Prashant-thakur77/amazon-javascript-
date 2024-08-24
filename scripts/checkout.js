801a8454cea92bdc031b225d3f18fbafaf7e8faa
PRASHANT THAKUR <prashant101007@gmail.com>
2024-08-24 11:44:16 +0530

    convert promises to async await

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,6 +5,26 @@ import { loadProducts ,loadProductsFetch} from "../data/products.js";
 import { loadCart } from "../data/cart.js";
 //import '../data/cart-class.js';
 //import '../data/backend-practice.js'
+
+async function loadPage(){
+  
+  await loadProductsFetch();
+  const value=await new Promise((resolve)=>{
+    loadCart(()=>{
+      resolve('value3');
+    });
+
+  });
+
+  renderOrderSummary();
+  renderPaymentSummary();
+ 
+
+}
+loadPage()
+
+
+/*
 Promise.all([
   loadProductsFetch(),
   new Promise((resolve)=>{
@@ -20,7 +40,7 @@ Promise.all([
   renderOrderSummary();
   renderPaymentSummary();
 
-});
+});*/
 
 /*new Promise((resolve)=>{
   
